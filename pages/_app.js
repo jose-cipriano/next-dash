8c574a6dce5438d10b019918fd0051752b672df5
jose-cipriano <webdev10250907@gmail.com>
2022-12-29 14:56:21 -0300

    remove console

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,6 @@ export default function App({ Component, pageProps }) {
     }
 
     const getLayout = Component.getLayout || ((page) => page)
-    console.log('test deploy')
 
     return (
         <SWRConfig
